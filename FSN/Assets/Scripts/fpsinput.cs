243e21842b163834cad3991e9c026d82f1339f4c
KoiKomei <alex.parisella@gmail.com>
2018-04-09 17:23:10 +0200

    Cambio dell'fpsinput con fpsinputjump
    C'è ancora fpsinput nella cartella

--- FSN/Assets/Scripts/fpsinput.cs
+++ FSN/Assets/Scripts/fpsinput.cs
@@ -12,18 +12,29 @@ public class fpsinput : MonoBehaviour {
         _char = GetComponent<CharacterController>();
 		
 	}
-	public float speed = 6.0f;
-    public float gravity = -9.8f;
+	private float speed = 6.0f;
+    private float jump = 8.0f;
+    private float gravity = -9.8f;
+    private Vector3 move = Vector3.zero;
+    bool flip = true;
 	// Update is called once per frame
 	void Update () {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
+        if (Input.GetButton("Fire3")) {
+            _char.transform.SetPositionAndRotation(new Vector3(0, 0, 0), new Quaternion(0,0,0,w:0));
+        }
 
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);
-        movement.y = gravity;
-        movement *= Time.deltaTime;
-        movement = transform.TransformDirection(movement);
-        _char.Move(movement);
+        deltaX = Input.GetAxis("Vertical") * (flip ? -1 : 1);
+        deltaX = deltaX * speed;
+        if (_char.isGrounded)
+        {
+            Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+            movement = Vector3.ClampMagnitude(movement, speed);
+            movement.y = gravity;
+            movement *= Time.deltaTime;
+            movement = transform.TransformDirection(movement);
+            _char.Move(movement);
+        }
 	}
 }
